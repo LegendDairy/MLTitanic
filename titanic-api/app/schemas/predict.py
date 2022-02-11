75423bd3a2acbe11e3009140e18608c0d2f0b053
fdescle <fdescle@deloitte.com>
2022-02-11 13:43:14 +0100

    api

---
+++ titanic-api/app/schemas/predict.py
@@ -0,0 +1,33 @@
+from typing import Any, List, Optional
+import numpy as np
+
+from classification_model.processing.validation import TitanicDataInputSchema
+from pydantic import BaseModel
+
+
+class PredictionResults(BaseModel):
+    errors: Optional[Any]
+    version: str
+    predictions: Optional[List[int]]
+
+
+class MultipleTitanicDataInputs(BaseModel):
+    inputs: List[TitanicDataInputSchema]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "inputs": [
+                    {
+                        "name": "Frédéric, Mr. D.",
+                        "pclass": 1,
+                        "age": 25.0,
+                        "sibsp": 0,
+                        "fare": 300,
+                        "sex": "male",
+                        "cabin": "E203",
+                        "embarked": "S",
+                    }
+                ]
+            }
+        }
